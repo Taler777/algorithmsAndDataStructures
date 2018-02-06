2731bd5b1aa322059d0aa806f3d0a828e20852a9
Alexey <taler2002@gmail.com>
2018-02-06 16:44:46 +0300

    sixth homework

---
+++ src/HomeWork6/Node.java
@@ -0,0 +1,10 @@
+package HomeWork6;
+
+public class Node<E> { // класс описывает элемент двоичного дерева
+    Node left; // указатель на левое поддерево
+    Node right; // указатель на правое поддерево
+    Node parent; // указатель на родителя
+    E fio;  // данные
+    E age;  // данные
+    int tabelNumber;  // данные
+}
